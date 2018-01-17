3e0cb86310152771fc0acbf37669064092cde4dc
maidinh <maidinh@postech.ac.kr>
2018-01-17 23:09:19 +0900

    auth ok

---
+++ express/server/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: '428043076742-c10bcrln69g3d1jl21o2uffdn3d99fkn.apps.googleusercontent.com',
+  googleClientSecret: 'Zq_JTJ_weKNx6s9g1UOt5Mjs',
+  // mongoURI: 'mongodb://bubu:bubu@ds259117.mlab.com:59117/bubu-test',
+  mongoURI: 'mongodb://127.0.0.1:27017/bubu-test',
+  cookieKey: 'aquabubu'
+}
